3e95a73d9f6d2bba45febabe14c180b55298eccb
alex cai <cyy0523xc@gmail.com>
2015-09-21 10:54:00 +0800

    yml cp

--- php-dev/yml-cp.sh
+++ php-dev/yml-cp.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # 配置文件初始化 
+# 
+# USAGES: ./yml-cp.sh np 
 
 
 start_type=
@@ -8,4 +10,15 @@ then
     start_type='-'$1
 fi
 
-cp docker-compose"$start_type".yml.example docker-compose.yml
+# 判断文件是否存在 
+from_file=./docker-compose"$start_type".yml.example
+if [ ! -f $from_file ]
+then
+    echo "ERROR: $from_file is not existed!"
+    exit 1
+fi
+
+to_file=./docker-compose.yml
+cp $from_file $to_file
+
+echo 'SUCCESS: Copy '$from_file' to '$to_file
