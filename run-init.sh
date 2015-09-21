c8e8ce7f230cdbcec61661f3f3b431e1fe60125c
alex cai <cyy0523xc@gmail.com>
2015-09-21 12:49:07 +0800

    php-dev config files

--- run-init.sh
+++ run-init.sh
@@ -44,5 +44,36 @@ else
     fi
 fi
 
+########################################
+### 初始化相关目录 #####################
+########################################
+
+[ ! -d /etc/nginx ] && sudo mkdir /etc/nginx
+[ ! -d /var/www ] && sudo mkdir /var/www
+[ ! -d /data ] && sudo mkdir /data
+
+#user=$(whoami)
+#sudo -R $user:$user /var/www
+
+########################################
+### 初始化PHP环境的相关配置 ############
+########################################
+
+cd ./php-dev/config/
+if [ ! -f php-fpm.conf ]
+then 
+    # 将example文件复制成正式配置文件
+    # 如果本地有特殊的配置，可以自己修改
+    file_list=$(ls *.example)
+    for from_file in $file_list 
+    do 
+        to_file=${from_file%.*}
+        [ ! -f $to_file ] && cp $from_file $to_file
+    done
+    echo "Finish: copy php-dev config files"
+else
+    echo "The php-dev config files is existed!"
+fi
+
 # 执行成功
 echo 'SUCCESS!'
