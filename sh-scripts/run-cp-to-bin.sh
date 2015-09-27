1206ba4eea4835788b5746d77ce1b099d175e08b
alex cai <cyy0523xc@gmail.com>
2015-09-27 17:03:03 +0800

    sudo命令是否存在的问题

--- sh-scripts/run-cp-to-bin.sh
+++ sh-scripts/run-cp-to-bin.sh
@@ -2,6 +2,11 @@
 # ./ibbd-docker-run每次修改之后，需要cp到/usr/local/bin/目录
 #
 
-sudo rm /usr/local/bin/ibbd-docker-run
-sudo cp ./ibbd-docker-run /usr/local/bin/ibbd-docker-run
+sudo=
+if which sudo; then 
+    sudo=sudo
+fi
+
+$sudo rm /usr/local/bin/ibbd-docker-run
+$sudo cp ./ibbd-docker-run /usr/local/bin/ibbd-docker-run
 
